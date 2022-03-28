c3b1fe975dabfc3dad8100663d27344494595c0f
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-03-29 02:24:57 +0530

    half part is left

--- src/component/HomePage/Homepage.jsx
+++ src/component/HomePage/Homepage.jsx
@@ -30,6 +30,89 @@ export const Homepage = () => {
             />
           </div>
         </div>
+        <div id="smallbox10">
+          <div id="smallbox11">
+            <div id="smallbox110">Market your business</div>
+            <div id="smallbox111">
+              Send the right messages on all the right channels. Build emails,
+              social ads, landing pages, postcards, and more from one place.
+            </div>
+            <button id="smallbox112">Marketing campaigns</button>
+          </div>
+          <div id="smallbox12">
+            <img
+              src="https://eep.io/images/yzco4xsimv0y/2EG9TpUR2jS1DuUxiwHQqN/56cf8275691b0f54e4d4a085233796fd/MTSB_Multichannel_Floats-KaFKcJgs.png?w=760&fm=webp&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div id="smallbox20">We’ll help you—</div>
+        <div id="smallbox30">
+          <div id="smallbox31">
+            <img
+              src="https://eep.io/images/yzco4xsimv0y/76b1ts3zfjk8TTYommiHJC/c95c27c98b6418ac8d3c240fae52ab1b/MTSB_Audience_Management_Floats__1_.png?w=760&fm=webp&q=80"
+              alt=""
+            />
+          </div>
+          <div id="smallbox12">
+            <div id="smallbox310">Reach the right people</div>
+            <div id="smallbox311">
+              Put your people at the heart of your marketing with tools that
+              help you get to know your audience and see who you should be
+              talking to.
+            </div>
+            <div id="smallbox312">Audience management tools</div>
+          </div>
+        </div>
+        <div id="smallbox30">
+          <div id="smallbox31">
+            <img
+              src="https://eep.io/images/yzco4xsimv0y/2GU9oK4zKAqkVIBHLLaCO3/d362c321e18b4e04573f714efdbc200c/MTSB_Creative_Assitant_Floats__1_.png?w=760&fm=webp&q=80"
+              alt=""
+            />
+          </div>
+          <div id="smallbox12">
+            <div id="smallbox310">Create better content</div>
+            <div id="smallbox311">
+              Let your brand shine through with easy-to-use the design tools and
+              flexible templates. Our AI-powered Creative Assistant will even
+              generate custom designs for you in seconds.
+            </div>
+            <div id="smallbox312">Explore content studio</div>
+          </div>
+        </div>
+        <div id="smallbox30">
+          <div id="smallbox31">
+            <img
+              src="https://eep.io/images/yzco4xsimv0y/1sAoiKpmzxdzN3BbfoOER4/bd50d14cc948046bb9b273b116221b8c/MTSB_Automate_Your_Market_Floats__1_.png?w=760&fm=webp&q=80"
+              alt=""
+            />
+          </div>
+          <div id="smallbox12">
+            <div id="smallbox310">Automate your marketing</div>
+            <div id="smallbox311">
+              Add a personal touch without the personal effort through automated
+              messages that reach your customers at exactly the right moments.
+            </div>
+            <div id="smallbox312">Learn about automations</div>
+          </div>
+        </div>
+        <div id="smallbox30">
+          <div id="smallbox31">
+            <img
+              src="https://eep.io/images/yzco4xsimv0y/5y4H4UxWokj0HCevMHusjk/0c14f2c8db03e1e4601667aad39931bf/MTSB_Send_Time_Optimization_Floats__1_.png?w=760&fm=webp&q=80"
+              alt=""
+            />
+          </div>
+          <div id="smallbox12">
+            <div id="smallbox310">Take action with our insights</div>
+            <div id="smallbox311">
+              With all your data and insights in one place, you can see what’s
+              working best and get recommendations to help you do more of it.
+            </div>
+            <div id="smallbox312">Insights & analytics tools</div>
+          </div>
+        </div>
       </div>
     </>
   );
