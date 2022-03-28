82806ba7a2b1e7095029f80fa46ce1d5479bc23e
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-03-29 01:32:56 +0530

    done some button prt

---
+++ src/component/HomePage/Homepage.jsx
@@ -0,0 +1,36 @@
+import "./homepage.css";
+export const Homepage = () => {
+  return (
+    <>
+      <div id="smallbox0">
+        <div id="smallbox00">
+          <div id="smallbox01">Do it all with Mailchimp</div>
+          <div id="smallbox02">
+            <p>
+              Bring your audience data, marketing channels, and insights
+              together so you can reach your goals faster—all from a single
+              platform.
+            </p>
+          </div>
+        </div>
+        <div id="smallbox10">
+          <div id="smallbox11">
+            <div id="smallbox110">Get your business online</div>
+            <div id="smallbox111">
+              Give your brand a home with a custom domain. Then launch a website
+              to sell products or take appointments, all with built-in marketing
+              tools to help you boost sales and find fans.
+            </div>
+            <button id="smallbox112">Websites and Stores</button>
+          </div>
+          <div id="smallbox12">
+            <img
+              src="https://eep.io/images/yzco4xsimv0y/2VXH4llvvqqyqtQJRyrnRH/05cc0990764352838f7a4540265a00ad/MTSB_Website_Builder_Floats-dbFKcJgs.png?w=760&fm=webp&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
