b2d32b1715ba0ae36cedf456dfec2d0cb5953b14
Faruk khan <fk300220@gmail.com>
2022-03-30 11:36:17 +0530

    b

--- footer/footer/src/compo/footerUp.jsx
+++ footer/footer/src/compo/footerUp.jsx
@@ -2,6 +2,33 @@
 
 export const FooterUp=()=>{
     return(
-        
+        <div>
+            <div>
+                <h1>Resources to give you the inside track</h1>
+                <p>Looking to get ahead? We have expert guidance, timely resources, and inspiring stories from entrepreneurs who’ve been in your shoes. Business-growing shoes.</p>
+            </div>
+            <div>
+                <div>
+                    <p>FREE GUIDE</p>
+                    <h1>E-commerce Time Savers</h1>
+                    <p>Use these powerful automations to drive customer loyalty and repeat business, while saving time.</p>
+                    <p>Learn More</p>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://eep.io/images/yzco4xsimv0y/185jQxfZHnXRFyd7qwhC3h/4a6116f0da1337f4c962e908ca335ac8/MCP_Going-Through-It_Logout_MCP-Homepage-Grid-Quadrant.jpg?w=780&fm=webp&q=80" alt="" />
+                        <img src="https://eep.io/images/yzco4xsimv0y/4GB4ccyPMbWUX4aYbzIlo9/5f045fc4120b8afb030036e3ce8136e9/mcp-horizontal.png?w=240&fm=webp&q=80" alt="" />
+                    </div>
+                    <div>
+                        <p>PODCASTS</p>
+                        <h2>Going Through It</h2>
+                        <p>Our new host Jenny Yang sits down with 15 notable guests to reflect on how advice from an elder at a pivotal moment helped define each guest’s life and career.</p>
+                        <p>Listen Now</p>
+                    </div>
+                </div>
+
+            </div>
+            
+        </div>
     )
 }
