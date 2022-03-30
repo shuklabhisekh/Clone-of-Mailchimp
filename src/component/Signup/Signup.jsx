dcb0f754e03f082f4e03eb7bd7a65a5c57891fa7
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-03-30 17:55:20 +0530

    login part is done

--- src/component/Signup/Signup.jsx
+++ src/component/Signup/Signup.jsx
@@ -1,5 +1,9 @@
 import "./Signup.css";
 export const Signup = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div id="smallbox0">
       <div id="smallbox1">
@@ -17,11 +21,46 @@ export const Signup = () => {
                 Create an account or
                 <span> log in</span>
               </div>
-              <form action="">
-                <label htmlFor="">Email</label>
+              <form id="form" action="" onSubmit={handleSubmit}>
+                <label id="email" htmlFor="">
+                  Email
+                </label>
                 <br />
-                <input type="text" />
+                <input type="text" id="emailInput" required />
+                <label id="email" htmlFor="">
+                  Username
+                </label>
+                <br />
+                <input type="text" id="emailInput" required />
+                <label id="email" htmlFor="">
+                  Password
+                </label>
+                <br />
+                <input type="text" id="emailInput" required />
+                <br />
+                <input id="checkbox" type="checkbox" name="" />
+                <label id="spamMail" htmlFor="">
+                  I don't want to receive <label>promotional emails</label> from
+                  Mailchimp.
+                </label>
+                <br />
+                <br />
+                <br />
+                <label htmlFor="" id="terms">
+                  By creating an account, you agree to our <span>Terms</span>{" "}
+                  and have read and acknowledge the{" "}
+                  <span>Global Privacy Statement</span> .
+                </label>
+                <br />
+                <br />
+                <br />
+                <input type="submit" value="Sign Up" name="" id="button" />
               </form>
+            </div>
+            <div id="credit">
+              ©2001–2022 All Rights Reserved. Mailchimp® is a registered
+              trademark of The Rocket Science Group, LLC. Cookie Preferences,
+              Privacy, and Terms.
             </div>
           </div>
         </div>
