d4f3108eda98ab203c5f7b2e9d8867a558a477ae
Akshay Jagtap <93571686+akshayjagtap1111@users.noreply.github.com>
2022-04-10 17:34:42 +0530

    added backend files

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+    user:"jagtapvicky1111@gmail.com",
+    clintId:"448499962015-ifoka12fhlurs4o83b9i4bi1j78amemh.apps.googleusercontent.com",
+    clintSecret:"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",
+    refreshToken:"1//04zwoBv0JnSECCgYIARAAGAQSNwF-L9IrBd3K0SetMPMauwOhyMCcMceCeOqEkaTAH-98yZJn8pZZDZEyS6bL0gAUmewm7Y0PiLo"
+}
